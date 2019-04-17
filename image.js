d80eed858203f8472fdd530b671bfb41e8bb8900
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-17 16:58:57 -0400

    Add image to planning area

--- image.js
+++ image.js
@@ -15,6 +15,30 @@ function addImage() {
         return;
     }
 
+    var newimg = new Image();
+    newimg.src = imgurl;
+    newimg.onload = loadImage;
+
+}
+
+
+function loadImage() {
+
+    this.classList.add("addedimg");
+    if(this.height > 100) {
+        var hscale = this.height/100;
+        this.width = this.width/hscale;
+        this.height = this.width/hscale;
+    }
+
+    if(this.width > 100) {
+        var wscale = this.width/100;
+        this.width = this.width/wscale;
+        this.height = this.width/wscale;
+    }
+
+    $("planarea").appendChild(this);
+    // hide err message if any
 
 }
 
